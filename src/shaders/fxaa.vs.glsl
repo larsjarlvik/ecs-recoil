cf139a057d1f68e1ac4a121850b253385ba1655e
larsjarlvik <lars.jarlvik@gmail.com>
2020-11-19 11:30:27 +0700

    Test FXAA-311

--- src/shaders/fxaa.vs.glsl
+++ src/shaders/fxaa.vs.glsl
@@ -1,26 +1,16 @@
-#version 300 es
+#version 100
 precision mediump float;
 
-layout(location = 0) in vec3 aVertex;
+attribute vec3 aVertex;
 
 uniform vec2 uResolution;
 
-out vec2 resolution;
-out vec2 rgbNW;
-out vec2 rgbNE;
-out vec2 rgbSW;
-out vec2 rgbSE;
-out vec2 rgbM;
+varying vec2 fragCoord;
+varying vec2 resolution;
 
 void main(void) {
-    vec2 fragCoord = (aVertex.xy + 1.0) * 0.5 * uResolution;
-    vec2 inverseVP = 1.0 / uResolution.xy;
-    rgbNW = (fragCoord + vec2(-1.0, -1.0)) * inverseVP;
-    rgbNE = (fragCoord + vec2( 1.0, -1.0)) * inverseVP;
-    rgbSW = (fragCoord + vec2(-1.0,  1.0)) * inverseVP;
-    rgbSE = (fragCoord + vec2( 1.0,  1.0)) * inverseVP;
-    rgbM = vec2(fragCoord * inverseVP);
-    resolution = uResolution;
+    fragCoord = (aVertex.xy + 1.0) * 0.5;
+    resolution = 1.0 / uResolution;
 
     gl_Position = vec4(aVertex, 1.0);
 }
